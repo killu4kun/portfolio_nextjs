95045910524cdbd571c4c9ce0cf1ef95e5ab3014
Matheus <mateeus.soarees@outlook.com>
2022-07-25 16:32:47 -0300

    imp : change the color pallete

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -8,11 +8,11 @@ const About = () => {
     <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#F806CC]">
+          <p className="uppercase text-xl tracking-widest text-[#0078AA]">
             About
           </p>
-          <p className="py-2 text-[#570A57]">/ / Just do it</p>
-          <p className="py-2 text-[#F806CC]">
+          <p className="py-2 text-[#3AB4F2]">/ / Just do it</p>
+          <p className="py-2 text-[#0078AA]">
             Fusce ligula urna, ultrices feugiat venenatis lacinia, elementum vel
             eros. Nunc laoreet enim risus, quis laoreet velit pharetra eu.
             Curabitur sollicitudin, elit eget blandit venenatis, purus nibh
@@ -24,7 +24,7 @@ const About = () => {
             lacinia cursus vel sed sem. Duis dignissim vitae purus eget tempus.
             Duis id orci consectetur dui tristique rutrum.
           </p>
-          <p className="py-2 text-[#A91079]">
+          <p className="py-2 text-[#0078AA]">
             Sed ac purus et libero egestas vehicula id in leo. Curabitur in
             laoreet tortor, in dignissim justo. Quisque tempus justo eu semper
             mollis. Duis accumsan a magna sit amet blandit. Aenean accumsan
@@ -37,12 +37,12 @@ const About = () => {
             vulputate lacinia tempus.
           </p>
           <Link href="/#projects">
-            <p className="py-2 text-[#F806CC] underline cursor-pointer">
+            <p className="py-2 text-[#F2DF3A] underline cursor-pointer">
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-[#570A57] rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+        <div className="w-full h-auto m-auto shadow-xl shadow-[#0078AA] rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image src={AboutImg} className="rounded-xl" alt="/" />
         </div>
       </div>
--- components/Main/index.jsx
+++ components/Main/index.jsx
@@ -9,11 +9,11 @@ const Main = () => {
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <h1 className="py-4 text-[#A91079]">
-            Matheus <span className="text-[#F806CC]"> &quot;Kilua&quot;</span> Soares
+          <h1 className="py-4 text-[#0078AA]">
+            Matheus <span className="text-[#3AB4F2]"> &quot;Kilua&quot;</span> Soares
           </h1>
-          <h1 className="py-2 text-[#F806CC]">Front-End Web Dev</h1>
-          <p className="py-4 text-[#A91079] sm:max-w-[70%] m-auto">
+          <h1 className="py-2 text-[#F2DF3A]">Front-End Web Dev</h1>
+          <p className="py-4 text-[#3AB4F2] sm:max-w-[70%] m-auto">
             Actually i&apos;m a full-stack enginneer but right now im totally focused in honing my front end skills to the max degree.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
@@ -23,7 +23,7 @@ const Main = () => {
               rel="noreferrer"
             >
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                <FaLinkedinIn color="#F806CC" />
+                <FaLinkedinIn color="#3AB4F2" />
               </div>
             </a>
             <a
@@ -32,17 +32,17 @@ const Main = () => {
               rel="noreferrer"
             >
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                <FaGithub color="#F806CC" />
+                <FaGithub color="#F2DF3A" />
               </div>
             </a>
             <Link href="/#contact">
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                <AiOutlineMail color="#F806CC" />
+                <AiOutlineMail color="#3AB4F2" />
               </div>
             </Link>
             <Link href="/resume">
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                <BsFillPersonLinesFill color="#F806CC" />
+                <BsFillPersonLinesFill color="#F2DF3A" />
               </div>
             </Link>
           </div>
